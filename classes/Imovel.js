dfe6bf17e6a5d1272332694d3425a61fab6b0292
HENRIQUE G PREZOTTI <henriqueprezotti@gmail.com>
2022-12-06 09:06:13 -0300

    Criação da classe Imovel.js

---
+++ classes/Imovel.js
@@ -0,0 +1,126 @@
+class Imovel{
+    #preco;
+    #areaTotal;
+    #corretor;
+    #logradouro;
+    #bairro;
+    #cidade;
+    #UF;
+    #CEP;
+
+    constructor(preco, areaTotal, corretor, logradouro, bairro, cidade, UF, CEP){
+        this.preco = preco;
+        this.areaTotal = areaTotal;
+        this.corretor = corretor;
+        this.logradouro = logradouro;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.UF = UF;
+        this.CEP = CEP;
+    }
+
+    //Preco
+    get preco(){
+        return this.#preco;
+    }
+    set preco(preco){
+        if (preco >= 0){
+            this.#preco = preco;
+            return preco;
+        }
+        this.#preco = 0.0;
+        return 0.0;
+    }
+
+    //Area Total
+    get areaTotal(){
+        return this.#areaTotal;
+    }
+    set areaTotal(areaTotal){
+        if (areaTotal >= 0){
+            this.#areaTotal = areaTotal;
+            return areaTotal;
+        }
+        this.#areaTotal = 0.0;
+        return 0.0;
+    }
+
+    //Corretor
+    get corretor(){
+        return this.#corretor;
+    }
+    set corretor(corretor){
+        if (corretor != null){
+            this.#corretor = corretor;
+            return corretor;
+        }
+        corretor = null;
+        return null;
+    }
+
+    //Logradouro
+    get logradouro(){
+        return this.#logradouro;
+    }
+    set logradouro(logradouro){
+        this.#logradouro = logradouro;
+        return logradouro;
+    }
+
+    //Bairro
+    get bairro(){
+        return this.#bairro;
+    }
+    set bairro(bairro){
+        this.#bairro = bairro;
+        return bairro;
+    }
+
+    //Cidade
+    get cidade(){
+        return this.#cidade;
+    }
+    set cidade(cidade){
+        this.#cidade = cidade;
+        return cidade
+    }
+
+    //UF
+    get UF(){
+        return this.#UF;
+    }
+    set UF(UF){
+        if (UF != "" && UF.length == 2){
+            this.#UF = UF;
+            return UF;
+        }
+        this.#UF = "XX";
+        return "XX";
+    }
+
+    //CEP
+    get CEP(){
+        return this.#CEP;
+    }
+    set CEP(CEP){
+        if (CEP != "" && CEP.length == 9){
+            this.#CEP = CEP;
+            return CEP;
+        }
+        this.#CEP = "000000000";
+        return "000000000";
+    }
+
+    precoTotal(){
+        return this.#preco = this.#preco + (this.#corretor.comissao / 100);
+    }
+
+    toString(){
+        return "Preço: " + this.precoTotal() + ";\n"
+            + "Área Total: " + this.#areaTotal + ";\n"
+            + "Corretor: " + this.#corretor.nome + ";\n"
+            + "Telefone: "+ this.#corretor.telefone + ";\n"
+            + "Endereço: " + this.#logradouro + ", " + this.#bairro + ", " + this.#cidade + " - " + this.#UF + ", CEP: " + this.#CEP + ";\n";
+    }
+
+}
