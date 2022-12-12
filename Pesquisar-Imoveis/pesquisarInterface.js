401b8f3eaa5f54ef6631e75fe0d267ba519c242b
Samir Coutinho <samircborges@gmail.com>
2022-12-12 20:04:05 -0300

    pesquisarInterface

---
+++ Pesquisar-Imoveis/pesquisarInterface.js
@@ -0,0 +1,48 @@
+var cbCasa = document.getElementById("cbCasa");
+var cbApartamento = document.getElementById("cbApartamento");
+var cbTerreno = document.getElementById("cbTerreno");
+var inAreaMinima = document.getElementById("inAreaMinima");
+var inAreaMaxima = document.getElementById("inAreaMaxima");
+var btLimpar = document.getElementById("btLimpar");
+var btFiltrar = document.getElementById("btFiltrar");
+
+
+function filtrarImoveis(){
+
+    let filtroArea = [];
+    let tipoImovel = [];
+
+    let areaMaxima = Number(inAreaMaxima.value);
+    let areaMinima = Number(inAreaMinima.value);
+
+    filtroArea.push(areaMinima > 0 ? areaMinima : 0);
+    filtroArea.push(areaMaxima > areaMinima ? areaMaxima : 0);
+
+    inAreaMinima.value = filtroArea[0];
+    inAreaMaxima.value = filtroArea[1] != 0 ? filtroArea[1] : "";
+
+    if (cbApartamento.checked) {
+        tipoImovel.push("Apartamento");
+    }
+    if (cbCasa.checked) {
+        tipoImovel.push("Casa");
+    }
+    if (cbTerreno.checked) {
+        tipoImovel.push("Terreno");
+    }
+}
+
+function limparFiltros() {
+
+    cbApartamento.checked = false;
+    cbCasa.checked = false;
+    cbTerreno.checked = false;
+
+    inAreaMinima.value = 0;
+    inAreaMaxima.value = "";
+
+}
+
+
+btFiltrar.addEventListener("click", filtrarImoveis);
+btLimpar.addEventListener("click", limparFiltros);
